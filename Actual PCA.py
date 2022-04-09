4d4d84ee5f3f850b4dd4bb965a4e1d3360f4edb9
JonathonCV <81706070+Bryozoan@users.noreply.github.com>
2022-04-08 18:47:28 -0600

    PCA working

--- Actual PCA.py
+++ Actual PCA.py
@@ -13,24 +13,83 @@ from sklearn.decomposition import PCA
 #import matplotlib to plot histograms
 import matplotlib.pyplot as plt
 
+#set the figure resolution much higher:
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['savefig.dpi'] = 500
+
 #import thinsection data from excel 
 dFthinSectionData = pd.read_excel(r'C:\Users\therobe7\OneDrive\SRC_and_Research\MountJoy\TestCorrect Timpoweap Data sheet.xlsx',\
                                   sheet_name = 'AllData');
 dFthinSectionData.set_index('Sample', inplace=True);
-print(dFthinSectionData)
+
+mLabels = pd.read_excel(r'C:\Users\therobe7\OneDrive\SRC_and_Research\MountJoy\TestCorrect Timpoweap Data sheet.xlsx',\
+                                  sheet_name = 'Labels')
+    
+#print(dFthinSectionData)
+
 #Normalize the data
 #scalerData = StandardScaler(dFthinSectionData);
+
+#Run the PCA
 pca = PCA(.70)
-pca.fit(dFthinSectionData);
-pThinSectionData = pca.transform(dFthinSectionData);
+pca.fit(dFthinSectionData)
+pThinSectionData = pca.transform(dFthinSectionData)
 print(pca.n_components_)
+
+
 fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(1,1,1) 
+ax = fig.add_subplot(1,1,1)
+ax.legend(labels=mLabels['Index'])
 ax.set_xlabel('Principal Component 1', fontsize = 15)
 ax.set_ylabel('Principal Component 2', fontsize = 15)
 ax.set_title('2 Component PCA', fontsize = 20)
 tPCA = pca.components_
+vVar = pca.explained_variance_ratio_
+print('Varience accounted by main principal componentes', vVar)
+
 #Plot the 2 PCA
-ax.scatter(pThinSectionData[0:8,0],pThinSectionData[0:8,1], c="blue", marker="o") 
-ax.scatter(pThinSectionData[9:37:,0],pThinSectionData[9:37,1], c="red", marker="x")
+ax.scatter(pThinSectionData[:,0],pThinSectionData[:,1], c=mLabels['Color'], marker="o") 
+
 plt.show()
+
+
+def depthLog(df, indicator, location, depth):
+    
+
+
+    
+    locID = df[location].unique()
+    dSize = locID.size
+    print('Locations:', locID)
+    
+    listEl = list()
+    listDep = list()
+
+    data = {'col':['b','r','g','c','m','blueviolet','teal','palevioletred','gold',\
+                      'darkgreen'],'mark':['o','s','^','p','*','d','X','>','<','v']}
+    sym = pd.DataFrame(data)
+    
+    
+    for i in range(dSize):
+        
+        dfNew = df.dropna(subset=[indicator])
+        
+        listEl.append(dfNew[indicator].where(dfNew[location].eq(locID[i])))
+        listDep.append(dfNew[depth].where(dfNew[location].eq(locID[i])))
+    
+        fig, ax = plt.plot(listEl[i], listDep[i], c=sym['col'].iloc[i], 
+                   label=(locID[i]))
+        plt.xlabel(indicator) 
+        plt.ylabel('Depth ')
+        plt.title(indicator + ' at outcrop %f' % locID[i])
+        plt.gca().invert_yaxis()
+        plt.show()
+
+df = pd.DataFrame(pThinSectionData, columns= ['PCA 1', 'PCA 2', 'PCA 3', 'PCA 4'])     
+df['Lateral Sort Order'] = mLabels['Lateral Sort order']
+df['Lateral Index'] = mLabels['Lateral Index']
+df['Vertical Sort Order'] = mLabels['Vertical Sort Order']
+df['Vertical Index'] = mLabels['Vertical Index']
+
+
+depthLog(df, 'PCA 1', 'Vertical Index', 'Vertical Sort Order')
---
+++ PCA3D.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 16:17:18 2022
+
+@author: therobe7
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+#import plotly to plot PCA
+#import matplotlib to plot histograms
+import matplotlib.pyplot as plt
+#import statsmodel for Kolmogorov-Smirnov test to determine if the samples came from the same distribution
+from scipy.stats import ks_2samp, kurtosis, skew
+
+#set the figure resolution much higher:
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['savefig.dpi'] = 500
+
+#import thinsection data from excel 
+dFthinSectionData = pd.read_excel(r'C:\Users\therobe7\OneDrive\SRC_and_Research\MountJoy\TestCorrect Timpoweap Data sheet.xlsx',\
+                                  sheet_name = 'AllData')
+dFthinSectionData.set_index('Sample', inplace=True);
+
+mLabels = pd.read_excel(r'C:\Users\therobe7\OneDrive\SRC_and_Research\MountJoy\TestCorrect Timpoweap Data sheet.xlsx',\
+                                  sheet_name = 'Labels')
+    
+#print(dFthinSectionData)
+
+#Normalize the data
+#scalerData = StandardScaler(dFthinSectionData)
+#scalerData = StandardScaler(dFthinSectionData.loc('Irregular fenestra':'Seafloor micrite'))
+
+#Perform PCA
+dVar = .70
+pca = PCA(dVar)
+pca.fit(dFthinSectionData)
+pThinSectionData = pca.transform(dFthinSectionData)
+print('Number of components:', pca.n_components_)
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1, projection='3d') 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_zlabel('Principal Component 3', fontsize = 15)
+ax.set_title('3 Component PCA', fontsize = 20)
+#tPCA = pca.components_
+
+
+## TODO 
+ax.scatter(pThinSectionData[:,0],pThinSectionData[:,1],pThinSectionData[:,2], c=mLabels['Color'], marker="o")
+#ax.scatter(pThinSectionData[0:8,0],pThinSectionData[0:8,1],pThinSectionData[0:8,2], c="blue", marker="o") # plot the point (2,3,4) on the figure
+#ax.scatter(pThinSectionData[9:28:,0],pThinSectionData[9:28,1],pThinSectionData[9:28,2],c="red", marker="x")
+plt.show()
+
+
+
